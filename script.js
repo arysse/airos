4c267d7cef8683476f92e1af482d3b335396ada7
arysse <123202822+arysse@users.noreply.github.com>
2023-04-16 12:08:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("mousemove", function(event) {
+  var circle = document.getElementById("circle");
+  var x = event.clientX;
+  var y = event.clientY;
+  circle.style.left = x + "px";
+  circle.style.top = y + "px";
+});
+
+document.addEventListener("mousemove", function(event) {
+  var circle = document.getElementById("circle2");
+  var x = event.clientX;
+  var y = event.clientY;
+  circle.style.left = x + "px";
+  circle.style.top = y + "px";
+});
